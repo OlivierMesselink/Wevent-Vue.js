c3aaa1fc18033691265386a6ad082bdc8afcf458
Olivier Messelink <oliviermesselink@gmail.com>
2021-05-11 11:09:32 +0200

    Removed default Vue.js content

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <section>
+    <img :src="imgLink" >
+  </section>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      imgLink: require('./assets/wevent_logo.png')
+    }
+  },
+}
+</script>
