80850c5fc7b883e6ab10484ba3ca5300d4709631
Olivier <oliviermesselink@gmail.com>
2021-06-02 12:02:25 +0200

    Fixed router.js conflict issues

--- src/router/index.js
+++ src/router/index.js
@@ -1,52 +1,28 @@
-<<<<<<< HEAD
-import { createRouter, createWebHistory } from "vue-router";
-import TheLandingPage from "../components/Pages/TheLandingPage.vue";
-import TheLoginPage from "../components/Pages/TheLoginPage.vue";
-import TheListPage from "../components/Pages/TheListPage.vue";
-import TheBusinessRegistration from "../components/Pages/TheBusinessRegistration/TheBusinessRegistration.vue";
-
-const routes = [
-  { path: "/", component: TheLandingPage, meta: { title: "Wevent.nl | Home" } },
-  {
-    path: "/login",
-    component: TheLoginPage,
-    meta: { title: "Wevent.nl | Login" },
-  },
-  {
-    path: "/search",
-    component: TheListPage,
-    meta: { title: "Wevent.nl | Zoekopdracht" },
-  },
-  {
-    path: "/business-registration",
-    component: TheBusinessRegistration,
-    meta: { title: "Wevent.nl | Je onderneming registreren" },
-  },
-];
-=======
 import { createRouter, createWebHistory } from 'vue-router'
 import TheLandingPage from '../components/Pages/TheLandingPage.vue';
 import TheLoginPage from '../components/Pages/TheLoginPage.vue';
 import TheListPage from '../components/Pages/TheListPage.vue';
 import TestPage from '../components/Pages/TestPage.vue';
 import TheAccountPage from '../components/Pages/TheAccountPage.vue';
-
-
-
-
+import TheBusinessRegistration from "../components/Pages/TheBusinessRegistration/TheBusinessRegistration.vue"
 
 const routes = [
   {path: '/', component: TheLandingPage, meta:{title: 'Wevent.nl | Home'}},
   {path: '/login', component: TheLoginPage, meta:{title: 'Wevent.nl | Login'}},
   {path: '/search', component: TheListPage, meta:{title: 'Wevent.nl | Zoekopdracht'}},
   {path: '/test', component: TestPage},
-  {path: '/account', component: TheAccountPage}
+  {path: '/account', component: TheAccountPage},
+  {
+    path: "/business-registration",
+    component: TheBusinessRegistration,
+    meta: { title: "Wevent.nl | Je onderneming registreren" },
+  },
 
 
 ]
->>>>>>> main
 
-const router = createRouter({
+
+const router = createRouter({ 
   history: createWebHistory(process.env.BASE_URL),
   routes,
 });
