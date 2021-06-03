2b2490115edfa65cfeb95aff67ddb72c348c1fd6
Pim van Schaik <pim.vanschaik@hotmail.nl>
2021-06-03 09:25:51 +0200

    Added explanation-comp, resolved issue /w transition group

--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,40 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import TheLandingPage from '../components/Pages/TheLandingPage.vue';
-import TheLoginPage from '../components/Pages/TheLoginPage.vue';
-import TheListPage from '../components/Pages/TheListPage.vue';
-import TestPage from '../components/Pages/TestPage.vue';
-import TheAccountPage from '../components/Pages/TheAccountPage.vue';
-import TheBusinessRegistration from "../components/Pages/TheBusinessRegistration/TheBusinessRegistration.vue"
+import { createRouter, createWebHistory } from "vue-router";
+import TheLandingPage from "../components/Pages/TheLandingPage.vue";
+import TheLoginPage from "../components/Pages/TheLoginPage.vue";
+import TheListPage from "../components/Pages/TheListPage.vue";
+import TestPage from "../components/Pages/TestPage.vue";
+import TheAccountPage from "../components/Pages/TheAccountPage.vue";
+import TheBusinessRegistration from "../components/Pages/TheBusinessRegistration/TheBusinessRegistration.vue";
 
 const routes = [
-  {path: '/', component: TheLandingPage, meta:{title: 'Wevent.nl | Home'}},
-  {path: '/login', component: TheLoginPage, meta:{title: 'Wevent.nl | Login'}},
-  {path: '/search', component: TheListPage, meta:{title: 'Wevent.nl | Zoekopdracht'}},
-  {path: '/test', component: TestPage},
-  {path: '/account', component: TheAccountPage},
+  { path: "/", component: TheLandingPage, meta: { title: "Wevent.nl | Home" } },
+  {
+    path: "/login",
+    component: TheLoginPage,
+    meta: { title: "Wevent.nl | Login" },
+  },
+  {
+    path: "/search",
+    component: TheListPage,
+    meta: { title: "Wevent.nl | Zoekopdracht" },
+  },
+  { path: "/test", component: TestPage },
+  { path: "/account", component: TheAccountPage },
   {
     path: "/business-registration",
     component: TheBusinessRegistration,
     meta: { title: "Wevent.nl | Je onderneming registreren" },
   },
+<<<<<<< Updated upstream
 ]
 
 
 const router = createRouter({ 
+=======
+];
+
+const router = createRouter({
+>>>>>>> Stashed changes
   history: createWebHistory(process.env.BASE_URL),
   routes,
 });
